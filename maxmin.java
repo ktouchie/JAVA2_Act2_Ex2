b3098977a2ee2c79a8d413b77535c1bdb6cafca8
Karin Touchie <ktouchie@gmail.com>
2015-12-22 11:51:49 +0100

    Declare array, variables

--- maxmin.java
+++ maxmin.java
@@ -3,7 +3,11 @@ class maxmin{
     
     public static void main(String[] args) {
         
+        int tab[13,6,7,2,10,9,16,1,20,12,3,15,8,18,4,0,5,14,2,19,11,17]
         
+        int min, max, i, j, a, b;
+        
+        for ()
         
     }
     
